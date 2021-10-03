964a8fafb570fa4ac37bc939a978656a482f001a
Takahiro720 <s.takahiro.720@gmail.com>
2021-10-03 18:52:09 +0900

    ・Sample Program No1 Add.

---
+++ 01_HelloWorld/01_HelloWorld.ino
@@ -0,0 +1,10 @@
+#include <M5Stack.h>
+
+void setup() {
+  M5.begin();           // M5Stackの初期化
+  M5.Power.begin();       // M5Stackの電源の初期化
+  M5.Lcd.print("Hello World");  // Hello Worldを表示
+}
+
+void loop() {
+}
